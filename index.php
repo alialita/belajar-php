af61e5b7d4e74f4b00d2a68885ba95420e2c7e59
Alia Anom Fretty Lita <aliaaflita@gmail.com>
2022-10-25 16:56:32 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Hello World!<br>" ; 
+
+$nama = "Alia Anom Fretty Lita ";
+$umur = 21;
+echo "Nama Saya <strong>$nama</strong>, saya berusia $umur tahun<br>";
+
+$namaAdik = "Ani";
+$umurAdik = 15;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun<br>";
+
+$selisihUmur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.";
+
+?>
