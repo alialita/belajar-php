6bee3f877f0a771de05a6a6621bf13c00f87dfeb
Alia Anom Fretty Lita <aliaaflita@gmail.com>
2022-11-26 00:19:18 +0700

    crud part 1

--- index.php
+++ index.php
@@ -1,17 +1,55 @@
 <?php
+//1. Buat koneksi dengan MYSQL
+$con = mysqli_connect("localhost","root","","fakultas");
 
-echo "Hello World!<br>" ; 
+//2. Cek konesksi dengan MySQL
+if(mysqli_connect_errno()){
+    echo "Koneksi gagal ". mysqli_connect_error();
+}else{
+    echo "Koneksi berhasil";
+}
 
-$nama = "Alia Anom Fretty Lita ";
-$umur = 21;
-echo "Nama Saya <strong>$nama</strong>, saya berusia $umur tahun<br>";
+//3. Membaca data dari table mysql
+$query = "SELECT * FROM mahasiswa";
 
-$namaAdik = "Ani";
-$umurAdik = 15;
+//4. Tampilkan data, dengan menjalankan sql query
+$result = mysqli_query($con,$query);
+$mahasiswa = [];
+if ($result) {
+    // tampilkan data per satu
+    while($row = mysqli_fetch_assoc($result)){
+        $mahasiswa[] = $row;
+    }
+    mysqli_free_result($result);
+}
 
-echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun<br>";
-
-$selisihUmur = $umur - $umurAdik;
-echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.";
+//5. Tutup koneksi mysql
+mysqli_close($con);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+
+    <table border="1" style="width:100%">
+        <tr>
+            <th>NIM</th>
+            <th>Nama</th>
+        </tr>
+        <?php foreach($mahasiswa as $value): ?>
+        <tr>
+            <td><?php echo $value["nim"]; ?></td>
+            <td><?php echo $value["nama"]; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
