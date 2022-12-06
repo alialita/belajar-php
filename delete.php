43a07c07e81ebf4c2d68a5da84523975973cc8bf
Alia Anom Fretty Lita <aliaaflita@gmail.com>
2022-12-06 21:01:32 +0700

    merubah delete dan update

--- delete.php
+++ delete.php
@@ -1,27 +1,29 @@
 <?php
-//Tangkap data dari form submit
+
+// tangkap data dari form submit
 if (isset($_GET['id'])){
     $id = $_GET['id'];
-    //Buat koneksi dengan MySQL
-     $con = mysqli_connect("localhost","root","","fakultas");
 
-    //Cek koneksi dengan MySQL
-    if (mysqli_connect_errno()) {
-        echo "Koneksi gagal". mysqli_connect_error();
-    }else{
-        echo "Koneksi berhasil";
-    }
-    //Buat SQL query untuk insert ke database
-    //Buat query untuk delete
-    $sql = "DELETE FROM mahasiswa WHERE id=$id";
+        // Buat koneksi dengan MySQL
+        $con = mysqli_connect("localhost","root","","fakultas");
 
-    //Jalankan query        
-    if (mysqli_query($con, $sql)) {
-        echo "Data berhasil dihapus";
-    } else {
-        echo "Terdapat kesalahan". mysqli_error($con);
-    }
+        // Check connection
+        if (mysqli_connect_errno()) {
+            echo "Koneksi gagal: " . mysqli_connect_error();
+            exit();
+        }else{
+            echo 'koneksi berhasil';
+        }
+
+        $sql = "DELETE FROM mahasiswa WHERE id='$id' ";
+
+        if (mysqli_query($con, $sql)) {
+            echo "Data berhasil dihapus";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($con);
+        }
           
-    mysqli_close($con);
-}
+        mysqli_close($con);
+    }
+
 ?>
